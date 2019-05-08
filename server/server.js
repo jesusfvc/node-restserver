6f76f79933b5e9925d273869790f6f34a06a4412
jesus valdez <jesus.fvc1@gmail.com>
2019-05-08 12:59:27 -0500

    Con modelo mvc

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 require('./config/config');
 
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 
+const app = express()
 
 const bodyParser = require('body-parser')
 
@@ -12,38 +13,16 @@ app.use(bodyParser.urlencoded({ extended: false }))
 //parse application/json
 app.use(bodyParser.json())
 
+app.use(require('./routes/usuario'));
 
-app.get('/usuarios', function (req, res) {
-    res.json('get Usuarios')
-})
 
-app.post('/usuarios', function (req, res) {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        });
-    } else {
-        res.json({
-            body
-        });
+mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true }, (error, res) => {
+    if (error) {
+        throw error;
     }
-})
+    console.log('Base de datos Online');
+});
 
-app.put('/usuarios/:id', function (req, res) {
-    let id = req.params.id;
-
-    res.json({
-        id
-    })
-})
-
-app.delete('/usuarios', function (req, res) {
-    res.json('delete Usuarios')
-})
 
 app.listen(process.env.PORT, () => {
     console.log("escuchando puerto " + process.env.PORT);
