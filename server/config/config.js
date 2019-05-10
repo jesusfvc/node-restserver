4b42ec24e6e5edb92ef9543b72f96af533b7ecd5
jesus valdez <jesus.fvc1@gmail.com>
2019-05-10 09:51:03 -0500

    restServerusuario

--- server/config/config.js
+++ server/config/config.js
@@ -18,10 +18,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // base de datos
 //======================
 let urlDB;
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else
-{
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
     urlDB = 'mongodb+srv://jesus:20021995@clusternode-4izo1.mongodb.net/cafe?retryWrites=true';
 }
 
